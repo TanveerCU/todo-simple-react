1f6a14e9217be2ef99f39f3b20b2b595b4e21894
tanveer <atanveer712@gmail.com>
2021-03-17 01:19:45 +0600

    deploy react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  
+  let [val,update] = useState([]);
+  let [id_val,id_update] = useState(0);
+  let [text,textUpdate] = useState("");
+
+function textChange(event){
+  textUpdate(event.target.value);
+}  
+
+function add(){
+  if(text !== ""){
+  textUpdate("");
+  id_update(prevValue=>prevValue+1);
+  update((prevValue)=>{
+   return [...prevValue, <div onClick={del} className="div-content " key={id_val}><i className="fas fa-trash-alt del" id={id_val} key={id_val}></i> {text}</div>];
+    });
+}
+function del(event){
+  if(event.target.id){
+    update((prevValue)=>{
+      prevValue = prevValue.map((elemnt)=>{
+       if(event.target.id != elemnt.key){
+         return elemnt;
+       }
+       else{
+         return "";
+       }
+      });
+      return prevValue;
+    });
+  }}
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 style={{textAlign:"center"}} className="main">TO DO APP</h1>
+    <div className = "box">
+    <div className="add-box">
+    <h1 className="entr">Entry</h1>
+    <div className="add-bar" onClick={add}>
+    <div className="entry-element">
+    <input className="input-bar" type="text" onChange={textChange} value={text} placeholder="Entry Here..."/>
+    <div className="entry-icon"><i className="fas fa-plus-circle main-icon"></i></div>
     </div>
+    </div>
+    {val}
+    </div>
+    </div>
+    </>
   );
 }
 
